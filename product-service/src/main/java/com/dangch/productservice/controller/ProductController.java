e1833c6c1c44ce4820d20403e19115cd6805a042
chudang94 <chudang94@gmail.com>
2024-07-05 11:31:04 +0700

    product service

---
+++ product-service/src/main/java/com/dangch/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.dangch.productservice.controller;
+
+import com.dangch.productservice.dto.ProductRequest;
+import com.dangch.productservice.dto.ProductResponse;
+import com.dangch.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
