3b24c2d074a78545ebbd2f0cdf34e2793b8d3277
Dang <chudang94@gmail.com>
2025-07-08 10:15:56 +0700

    new inventory service

---
+++ inventory/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+create table IF NOT EXISTS `t_inventory` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `sku_code` varchar(255) ,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
